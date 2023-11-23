0a9e99b97223e0d7c3189b32e1a4836ca9d8aae6
Mirel Korajac <mirelkorajac@gmail.com>
2023-11-23 22:13:03 +0100

    Implement Navigation in App.js

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet } from "react-native";
+
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import HomeScreen from "./screens/HomeScreen";
+import NotificationScreen from "./screens/NotificationScreen";
+import themeContext from "./theme/themeContext";
+import { DarkMode, LightMode } from "./navigation/navigationTheme";
+
+const Stack = createStackNavigator();
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Notifications"
+        component={NotificationScreen}
+        options={{ title: false }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
+  const [theme, setTheme] = useState("Light");
+
+  const themeData = { theme, setTheme };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <themeContext.Provider value={themeData}>
+      <NavigationContainer theme={theme === "Light" ? DarkMode : LightMode}>
+        <StackNavigator />
+      </NavigationContainer>
+    </themeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
